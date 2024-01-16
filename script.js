06d876cb66fb105e15e3e62ff292490065f61ee1
alejopandolfi <pandolfialejo@gmail.com>
2024-01-15 23:30:02 -0300

    mejoras

--- script.js
+++ script.js
@@ -26,3 +26,12 @@ anime({
     duration: 1000,
     easing: 'easeInOutQuad'
 });
+
+// Agrega y quita la clase 'animate-header' cada 5 segundos
+// Este script es opcional, lo utilizo para cambiar la velocidad cada vez que se carga la página
+// Este script es opcional, lo utilizo para cambiar la velocidad cada vez que se carga la página
+document.addEventListener('DOMContentLoaded', function () {
+    var speed = Math.random() * (2 - 1) + 1; // Velocidad aleatoria entre 1 y 2
+    document.getElementById('header-lines').style.animationDuration = speed + 's';
+});
+
